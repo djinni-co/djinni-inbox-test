3ef81a67254353495ee6c89bd6b5560080644f10
serhiivashchenko <serhii.vashchenko@exfo.com>
2023-11-07 12:28:22 -0800

    Scoring system

--- app/sandbox/views.py
+++ app/sandbox/views.py
@@ -2,22 +2,23 @@ from django.http import HttpResponse
 from django.db.models import Count, Q
 from django.shortcuts import render
 
-
-from .models import Recruiter, MessageThread
+from .models import Recruiter, MessageThread, JobPosting, Candidate
 
 # Hardcode for logged in as recruiter
 RECRUITER_ID = 125528
 
-def inbox(request):
-    recruiter = Recruiter.objects.get(id = RECRUITER_ID)
-    threads = MessageThread.objects.filter(recruiter = recruiter).select_related('candidate', 'job')
 
-    _context = { 'title': "Djinni - Inbox", 'recruiter': recruiter, 'threads': threads }
+def inbox(request):
+    recruiter = Recruiter.objects.get(id=RECRUITER_ID)
+    threads = MessageThread.objects.filter(recruiter=recruiter).select_related('candidate', 'job')
+
+    _context = {'title': "Djinni - Inbox", 'recruiter': recruiter, 'threads': threads}
 
     return render(request, 'inbox/chats.html', _context)
 
+
 def inbox_thread(request, pk):
-    thread = MessageThread.objects.get(id = pk)
+    thread = MessageThread.objects.get(id=pk)
     messages = thread.message_set.all().order_by('created')
 
     _context = {
@@ -29,3 +30,27 @@ def inbox_thread(request, pk):
     }
 
     return render(request, 'inbox/thread.html', _context)
+
+
+def jobs(request):
+    jobPosting = JobPosting.objects.all()
+    _context = {'title': "Djinni - Jobs", 'jobPosting': jobPosting}
+
+    return render(request, 'jobs/job_list.html', _context)
+
+
+def jobs_candidates(request, pk):
+    job = JobPosting.objects.get(id=pk)
+    candidates = Candidate.objects.all()
+    english_levels = ['basic', 'pre', 'intermediate', 'upper', 'fluent']
+
+    for candidate in candidates:
+        candidate.score = candidate.experience_years
+        candidate.score += english_levels.index(candidate.english_level)
+        if job.primary_keyword == candidate.primary_keyword:
+            candidate.score += 5
+
+
+    _context = {'title': "Djinni - Candidates", 'job': job, 'candidates': sorted(candidates, key=lambda x: x.score, reverse=True)}
+
+    return render(request, 'jobs/job_perfect_candidates.html', _context)
