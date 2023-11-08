dd810071c0d8cdbbe535af401ec9168c3da3af7a
SavingFrame <savingframe@gmail.com>
2023-11-08 11:00:59 +0100

    - Add celery to docker compose - Add signal for a run task with updating score rating

--- app/sandbox/apps.py
+++ app/sandbox/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SandboxConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sandbox'
+
+    def ready(self):
+        import sandbox.signals  # noqa
---
+++ app/sandbox/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from sandbox.models import MessageThread
+from sandbox.tasks import update_message_thread_scoring_task
+
+
+@receiver(post_save, sender=MessageThread)
+def update_message_thread_score(sender, instance, created, **kwargs):
+    update_message_thread_scoring_task.s(instance.id).apply_async()
