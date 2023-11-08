0eede9d843eb9a4aee3d0bdcf325da9dfc920213
Denys_Burko <denys_burko@epam.com>
2023-11-08 13:40:48 +0200

    Added score algorithm

--- app/sandbox/views.py
+++ app/sandbox/views.py
@@ -2,22 +2,28 @@ from django.http import HttpResponse
 from django.db.models import Count, Q
 from django.shortcuts import render
 
-
 from .models import Recruiter, MessageThread
+from .scoring import score_thread
 
 # Hardcode for logged in as recruiter
 RECRUITER_ID = 125528
 
-def inbox(request):
-    recruiter = Recruiter.objects.get(id = RECRUITER_ID)
-    threads = MessageThread.objects.filter(recruiter = recruiter).select_related('candidate', 'job')
 
-    _context = { 'title': "Djinni - Inbox", 'recruiter': recruiter, 'threads': threads }
+def inbox(request):
+    sort = request.GET.get('sort')
+    recruiter = Recruiter.objects.get(id=RECRUITER_ID)
+    threads = MessageThread.objects.filter(recruiter=recruiter).select_related('candidate', 'job')
+
+    if sort == 'score':
+        threads = sorted({score_thread(thread) for thread in threads}, key=lambda thread: thread.score, reverse=True)
+
+    _context = {'title': "Djinni - Inbox", 'recruiter': recruiter, 'threads': threads}
 
     return render(request, 'inbox/chats.html', _context)
 
+
 def inbox_thread(request, pk):
-    thread = MessageThread.objects.get(id = pk)
+    thread = MessageThread.objects.get(id=pk)
     messages = thread.message_set.all().order_by('created')
 
     _context = {
@@ -25,7 +31,7 @@ def inbox_thread(request, pk):
         'title': "Djinni - Inbox",
         'thread': thread,
         'messages': messages,
-        'candidate': thread.candidate,
+        'candidate': thread.candidate
     }
 
     return render(request, 'inbox/thread.html', _context)
