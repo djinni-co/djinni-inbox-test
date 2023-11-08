7ae79bbe009184ef93555d54486e5809568a9390
tempdeltavalue <mykhailo.mel1122@gmail.com>
2023-11-08 19:39:29 +0200

    Removed manual stuff, finished faiss vector search

--- app/sandbox/analyzator.py
+++ app/sandbox/analyzator.py
@@ -1,112 +1,73 @@
-import base64
-import datetime
 import time
 import faiss
 from sentence_transformers import SentenceTransformer, util
-from .models import MessageThread, JobPosting
+
+from django.db import models
+from .models import MessageThread
 
 class Analyzator:
     def __init__(self):
         self.hg_sentence_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
         self.faiss_index = None
 
-    def calc_priority(self, thread: MessageThread):
-        res_score = 0
-        job, candidate = thread.job, thread.candidate
-
-        # Manual criterions
-        # remote criterion
-        remote_criterion = job.remote_type == JobPosting.RemoteType.OFFICE and job.location == candidate.location or candidate.can_relocate
-        if remote_criterion:
-            res_score += 5
-
-        # uninterested_company_type_criterion
-        if job.company_type is not None and candidate.uninterested_company_types is not None:
-            uninterested_company_type_criterion = job.company_type in candidate.uninterested_company_types
-            if uninterested_company_type_criterion:
-                res_score -= 5
-
-        # english_level_criterion
-        english_level_criterion = job.english_level == candidate.english_level
-        if english_level_criterion:
-            res_score += 5
-        ####
-
-        # intersection of properties with same name
-        prop_names_intersection = set(job.__dict__.keys()).intersection(candidate.__dict__.keys())
-        prop_names_intersection.remove("_state")
-
-        job_inter_attrs = self.get_inter_attr(prop_names_intersection, job)
-        candidate_inter_attrs = self.get_inter_attr(prop_names_intersection, candidate)
-        res_score += self.IOU(job_inter_attrs, candidate_inter_attrs)
-
-        return res_score
-
-    def get_candidate_job_distance(self, thread: MessageThread):
+    def get_candidate_job_similarity(self, thread: MessageThread):
         messages = thread.message_set.all().order_by('created')
-        # add all messages
-        acc_candidate_data = " ".join(msg.body for msg in messages if msg.body is not None)
 
-        # add extra info
-        if thread.candidate.skills_cache is not None and thread.candidate.moreinfo is not None:
-            acc_candidate_data += thread.candidate.skills_cache + " " + thread.candidate.moreinfo
+        acc_candidate_str = self.convert_model_to_string(thread.candidate)
 
-        acc_candidate_data = acc_candidate_data.replace('\n', '')  # preprocess
+        # Should I add messages here?
+        acc_candidate_str += " ".join(msg.body for msg in messages if msg.body is not None)
 
-        emb1 = self.hg_sentence_model.encode(acc_candidate_data, convert_to_tensor=True)
-        emb2 = self.hg_sentence_model.encode(thread.job.long_description, convert_to_tensor=True)
+        job_str = self.convert_model_to_string(thread.job)
 
-        distance = util.pytorch_cos_sim(emb1, emb2)
-        return round(distance.numpy()[0][0], 3)
+        emb1 = self.hg_sentence_model.encode(acc_candidate_str, convert_to_tensor=True)
+        emb2 = self.hg_sentence_model.encode(job_str, convert_to_tensor=True)
+
+        similarity = util.pytorch_cos_sim(emb1, emb2)
+        return round(similarity.numpy()[0][0], 3)
 
     # utils
-    def get_inter_attr(self, names_inter, item):
-        res = []
-        for x in names_inter:
-            attr = getattr(item, x)
-            if x is isinstance(attr, datetime.date):
-                x = x.strftime('%m/%d/%Y')
 
-            res.append(attr)
+    def convert_model_to_string(self, model: models.Model):
+        model_str = str(model.__dict__)
 
-        return res
+        # primitive preprocess
+        model_str.replace("\n", " ")
+        model_str.replace("?", " ")
+        model_str.replace("r", " ")
+        #
+        return model_str
 
-    def IOU(self, list1, list2):
-        list1 = set(list1)
-        list2 = set(list2)
-
-        intersection = len(list1.intersection(list2))
-        union = len(list1.union(list2))
-        return intersection / union
 
     #FAISS
     def read_faiss_index(self, faiss_index_path):
         self.faiss_index = faiss.read_index(faiss_index_path)
 
-    def faiss_search(self, text, k=10):
-        text = text.replace('\n', ' ')  # preprocess
+    def faiss_search(self,  job: models.Model, k=10):
+        job_str = self.convert_model_to_string(job)
 
-        embeddings = self.hg_sentence_model.encode(text, convert_to_tensor=True)
+        embeddings = self.hg_sentence_model.encode(job_str, convert_to_tensor=True)
         D, I = self.faiss_index.search(embeddings.reshape(1, embeddings.shape[0]), k)
 
         return D, I
 
-
     def create_faiss_index(self, threads: [MessageThread], index_name):
         ids = []
         candidates_text_data = []
+
         for index, thread in enumerate(threads):
-            messages = thread.message_set.all().order_by('created')  # this is can be changeable  with time !!!
-            text = " ".join(msg.body for msg in messages if msg.body is not None)
+            messages = thread.message_set.all().order_by('created')
+            acc_candidate_str = self.convert_model_to_string(thread.candidate)
 
-            if thread.candidate.skills_cache is not None and thread.candidate.moreinfo is not None:
-                text = thread.candidate.skills_cache + " " + thread.candidate.moreinfo
+            # Should I add messages here? (message is changeable data)
+            acc_candidate_str += " ".join(msg.body for msg in messages if msg.body is not None)
 
-                text = text.replace('\n', ' ')  # preprocess
-                candidates_text_data.append(text)
+            candidates_text_data.append(acc_candidate_str)
 
-                ids.append(index) # id should be here (ids should be int) and it's incorrect to use index but I cannot unhash int value
+            ids.append(index) # id should be here (ids should be int) and it's incorrect to use index but I cannot unhash int value
 
+
+        # Inference
         batch_size = 10
         emb_shape = 384
         index = faiss.IndexFlatL2(emb_shape)
@@ -118,8 +79,8 @@ class Analyzator:
             index2.add_with_ids(embeddings, ids[i:i+batch_size])
             print(f"batch {i} from {len(candidates_text_data)}")
 
-        end_time = time.time() - start_time
-        print("Time to create embs ", end_time-start_time)
+        exec_time = time.time() - start_time
+        print("Time to create embs ", exec_time)
 
         faiss.write_index(index2, f'{index_name}')
 
--- app/sandbox/views.py
+++ app/sandbox/views.py
@@ -4,27 +4,38 @@ from django.shortcuts import render
 
 from .analyzator import Analyzator
 
-from .models import Recruiter, MessageThread
+from .models import Recruiter, MessageThread, JobPosting
 
 # Hardcode for logged in as recruiter
 RECRUITER_ID = 125528
 
-faiss_index_path = "faissIndexIDMap3.index"
+faiss_index_path = "faissIDMap.index"
 analyzator = Analyzator()
+analyzator.read_faiss_index(faiss_index_path)
+
+TEST_THREAD_INDEX = 0  # needed to select job posting for test
 
 def inbox(request):
     recruiter = Recruiter.objects.get(id = RECRUITER_ID)
     threads = MessageThread.objects.filter(recruiter = recruiter).select_related('candidate', 'job')
 
-    threads = sorted(threads, key=lambda x: analyzator.calc_priority(x), reverse=False)
+    # Needed to match faiss index ids (check create_faiss_index func)
+    threads = sorted(threads, key=lambda x: x.candidate.name, reverse=False)
 
-    # Only during first run and creates faiss index
-    if os.path.exists(faiss_index_path) is False:
-        analyzator.create_faiss_index(threads, faiss_index_path)
+    # Uncomment these lines to create new faiss index
+    # if os.path.exists(faiss_index_path) is False:
+    #   analyzator.create_faiss_index(threads, faiss_index_path)
 
-    analyzator.read_faiss_index(faiss_index_path)
-    # D, I = analyzator.faiss_search(threads[0].job.long_description)
-    # print(I)
+    # Inbox based on recruiter-candidate communication
+    # but we need to select job for candidates search
+    selected_job = threads[TEST_THREAD_INDEX].job
+    print("SELECTED JOB: \n", selected_job.long_description)
+    ####
+
+    distances, indices = analyzator.faiss_search(selected_job, k=1000)
+    threads = list(map(threads.__getitem__, indices[0]))
+
+    threads = map(lambda x:x[1], sorted(enumerate(threads), key=lambda x: distances[0][x[0]], reverse=False)) # sort threads by distance
 
     _context = { 'title': "Djinni - Inbox", 'recruiter': recruiter, 'threads': threads }
 
@@ -34,7 +45,7 @@ def inbox_thread(request, pk):
     thread = MessageThread.objects.get(id = pk)
     messages = thread.message_set.all().order_by('created')
 
-    distance = analyzator.get_candidate_job_distance(thread)
+    similarity = analyzator.get_candidate_job_similarity(thread)
 
     _context = {
         'pk': pk,
@@ -42,7 +53,7 @@ def inbox_thread(request, pk):
         'thread': thread,
         'messages': messages,
         'candidate': thread.candidate,
-        'distance': distance,
+        'similarity': similarity,
     }
 
     return render(request, 'inbox/thread.html', _context)
