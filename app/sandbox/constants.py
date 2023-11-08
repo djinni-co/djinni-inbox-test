e400390cca2aa01b796792be9a12e72640999917
Nick Tropin <posixivist32@gmail.com>
2023-11-08 17:29:24 +0200

    MVP Algorithm v.1 implementation
    Candidate's salary and experience are considered while skills evaluation
    is NIY

--- app/sandbox/constants.py
+++ app/sandbox/constants.py
@@ -3,32 +3,41 @@ import re
 class paging:
     INBOX = 5
 
+class templates:
+    class html:
+        class id:
+            CHAT_LIST = 'chat-list'
+            ADVANCED_SORTING_TOOL = 'adv-sorting-tool'
+
+        class name:
+            JOB_FILTER = 'job-filter'
+
 class sorting:
     class id:
         RECENT   = 'recent'
         ADVANCED = 'adv'
 
-        SALARY_LOW_HIGH = 'sal-lh'
-        SALARY_HIGH_LOW = 'sal-hl'
-
-        EXPERIENCE_LOW_HIGH = 'exp-lh'
-        EXPERIENCE_HIGH_LOW = 'exp-hl'
-
     class text:
         RECENT   = 'Most Recent First'
         ADVANCED = 'Advanced Sorting'
 
-        SALARY_LOW_HIGH = 'Salary Low-High'
-        SALARY_HIGH_LOW = 'Salary High-Low'
-
-        EXPERIENCE_LOW_HIGH = 'Experience Low-High'
-        EXPERIENCE_HIGH_LOW = 'Experience High-Low'
-
 class inbox:
+    class html:
+        class id:
+            CHAT_LIST  = templates.html.id.CHAT_LIST
+            ADVANCED_SORTING_TOOL = templates.html.id.ADVANCED_SORTING_TOOL
+
     class http:
         class param:
-            SORTING = 's'
+            SORTING = 'so'
             PAGING = 'p'
+            JOB_FILTER = templates.html.name.JOB_FILTER
+
+            # AST - Advanced Sorting Tool
+            class ast:
+                SALARY     = 'ast-salary'
+                EXPERIENCE = 'ast-experience'
+                SKILLS     = 'ast-skills'
 
     class paging:
         PER_PAGE = paging.INBOX
@@ -38,32 +47,20 @@ class inbox:
             id   = sorting.id.RECENT
             text = sorting.text.RECENT
 
-        class salary_low_high:
-            id   = sorting.id.SALARY_LOW_HIGH
-            text = sorting.text.SALARY_LOW_HIGH
-
-        class salary_high_low:
-            id   = sorting.id.SALARY_HIGH_LOW
-            text = sorting.text.SALARY_HIGH_LOW
-
-        class experience_low_high:
-            id   = sorting.id.EXPERIENCE_LOW_HIGH
-            text = sorting.text.EXPERIENCE_LOW_HIGH
-
-        class experience_high_low:
-            id   = sorting.id.EXPERIENCE_HIGH_LOW
-            text = sorting.text.EXPERIENCE_HIGH_LOW
-
         class advanced:
             id   = sorting.id.ADVANCED
             text = sorting.text.ADVANCED
 
         DEFAULT = recent
-        LIST = [
+        AVAILABLE_METHODS = [
             recent,
-            salary_low_high,
-            salary_high_low,
-            experience_low_high,
-            experience_high_low,
             advanced,
         ]
+
+        @classmethod
+        def str2obj (cls, string):
+            for sort_order in cls.AVAILABLE_METHODS:
+                if string in [sort_order.id, sort_order.text]:
+                    return sort_order
+
+            return None
--- app/sandbox/views.py
+++ app/sandbox/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect, QueryDict
-from django.db.models import Count, Q
-from django.shortcuts import render, reverse
+from django.db.models import Count, Q, Avg, F, Max, Min, Window
+from django.shortcuts import render, reverse, get_object_or_404
 
 from . import constants as C
 
@@ -9,27 +9,126 @@ from .models import Recruiter, MessageThread, JobPosting
 # Hardcode for logged in as recruiter
 RECRUITER_ID = 125528
 
+def _apply_advanced_sorting (threads, weights):
+    # FIXME: delete the limitation
+    # threads = threads.filter(job_id = 550484)
+
+    window = {
+       'partition_by': [F('job_id')],
+       # 'order_by': F('job_id').asc(),
+    }
+
+    thrs_ext = threads.annotate(
+        cand_min_sal = Window(
+            expression = Min('candidate__salary_min'), **window,
+        ),
+
+        cand_max_sal = Window(
+            expression = Max('candidate__salary_min'), **window,
+        ),
+
+        cand_min_exp = Window(
+            expression = Min('candidate__experience_years'), **window,
+        ),
+
+        cand_max_exp = Window(
+            expression = Max('candidate__experience_years'), **window,
+        ),
+
+        # exp_score = Window(
+        #     expression = (
+        #           F('candidate__experience_years')
+        #         - F('cand_min_exp')
+        #     ), **window,
+        # ),
+        # # (cand.experience_years - :exp_min)/:exp_range
+    )
+
+    for thr in thrs_ext:
+        thr.scores = {}
+
+        exp_range = thr.cand_max_exp - thr.cand_min_exp
+        thr.scores['experience'] = (
+            thr.candidate.experience_years - thr.cand_min_exp
+        ) / exp_range
+
+        sal_range = thr.cand_max_sal - thr.cand_min_sal
+        thr.scores['salary'] = (
+            thr.candidate.salary_min - thr.cand_min_sal
+        ) / sal_range
+
+        # TODO: Skills measuring is NIY
+        thr.scores['skills'] = weights['skills'] * 0
+
+        thr.scores['total'] = (
+              thr.scores['experience'] * weights['experience']
+            - thr.scores['salary'] * weights['salary']
+            + thr.scores['skills'] * weights['skills']
+        )
+
+    # from the highest score to the lowest
+    return sorted(thrs_ext, key = lambda v: v.scores['total'],
+                     reverse = True)
+
 def inbox(req):
+    context = { 'const': C.inbox }
+
     get_params = QueryDict('', mutable=True)
     get_params.update(req.GET)
 
     page = int(req.GET.get(C.inbox.http.param.PAGING) or 1)
-    sorting = req.GET.get(
-        C.inbox.http.param.SORTING
-    ) or C.inbox.sorting.DEFAULT
+    sorting = req.GET.get( C.inbox.http.param.SORTING )
+    sorting = C.inbox.sorting.str2obj(sorting) or C.inbox.sorting.DEFAULT
+
+    current_job = None
+    job_filter = int( req.GET.get( C.inbox.http.param.JOB_FILTER ) or -1)
+    if job_filter != -1:
+        current_job = get_object_or_404(JobPosting, pk = job_filter)
+        context |= { 'current_job': current_job }
 
     recruiter = Recruiter.objects.get(id = RECRUITER_ID)
-    threads = MessageThread.objects.filter(
-        recruiter = recruiter
-    ).select_related('candidate', 'job')[
+    threads = MessageThread.objects.filter( recruiter = recruiter )
+
+    if current_job:
+        threads = threads.filter( job__id = job_filter )
+
+    threads = threads.select_related('candidate', 'job')
+
+    if sorting is C.inbox.sorting.recent:
+        pass
+    elif sorting is C.inbox.sorting.advanced:
+        # The main deal
+        wt_salary     = get_params.get(C.inbox.http.param.ast.SALARY)
+        wt_experience = get_params.get(C.inbox.http.param.ast.EXPERIENCE)
+        wt_skills     = get_params.get(C.inbox.http.param.ast.SKILLS)
+
+        if None not in [wt_salary, wt_experience, wt_skills]:
+            context |= {
+                'ast_salary': wt_salary,
+                'ast_experience': wt_experience,
+                'ast_skills': wt_skills,
+            }
+
+            wt_salary     = int(wt_salary) / 100
+            wt_experience = int(wt_experience) / 100
+            wt_skills     = int(wt_skills) / 100
+
+            if None not in [wt_salary, wt_experience, wt_skills]:
+                threads = _apply_advanced_sorting(threads, {
+                    'salary': wt_salary,
+                    'experience': wt_experience,
+                    'skills': wt_skills,
+                })
+    else:
+        raise ValueError('Sorting algorithm is incorrect!')
+
+    threads = threads[
           (page - 1) * C.inbox.paging.PER_PAGE
         : page * C.inbox.paging.PER_PAGE
     ]
 
-    context = { 'const': C.inbox }
-
     context |= {
-        'sorting_options': C.inbox.sorting.LIST,
+        'sorting_options': C.inbox.sorting.AVAILABLE_METHODS,
         'current_sorting': sorting,
     }
 
@@ -42,8 +141,9 @@ def inbox(req):
     }
 
     context |= {
+        # FIXME: filter out Jobs that are not referenced by any MessageThread
         'jobs': JobPosting.objects.filter(
-            recruiter = recruiter
+            recruiter = recruiter,
         ).all()
     }
 
