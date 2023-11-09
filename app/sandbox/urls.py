ac63ac1458642fbafd1bf09c216123bee4ccea25
Vadym Volkov <vadymvolkov@Vadyms-MacBook-Pro.local>
2023-11-09 14:54:08 +0200

    changes to calculate job relevance

--- app/sandbox/urls.py
+++ app/sandbox/urls.py
@@ -4,7 +4,9 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-	path('', RedirectView.as_view(url='/inbox', permanent=False), name='root-redirect'),
-  path('inbox/', views.inbox, name='inbox'),
-  path('inbox/<pk>/', views.inbox_thread, name='inbox_thread'),
+    path('', RedirectView.as_view(url='/inbox', permanent=False), name='root-redirect'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('inbox/<pk>/', views.inbox_thread, name='inbox_thread'),
+    path('posted_jobs/', views.posted_jobs, name='jobs'),
+    path('jobs_match/<pk>/', views.jobs_candidates, name='jobs_candidates'),
 ]
--- app/sandbox/views.py
+++ app/sandbox/views.py
@@ -1,23 +1,25 @@
 from django.http import HttpResponse
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F, ExpressionWrapper, FloatField
 from django.shortcuts import render
 
-
-from .models import Recruiter, MessageThread
+from .models import Recruiter, MessageThread, JobPosting, Candidate
+from .match_scores import calculate_match_score
 
 # Hardcode for logged in as recruiter
 RECRUITER_ID = 125528
 
-def inbox(request):
-    recruiter = Recruiter.objects.get(id = RECRUITER_ID)
-    threads = MessageThread.objects.filter(recruiter = recruiter).select_related('candidate', 'job')
 
-    _context = { 'title': "Djinni - Inbox", 'recruiter': recruiter, 'threads': threads }
+def inbox(request):
+    recruiter = Recruiter.objects.get(id=RECRUITER_ID)
+    threads = MessageThread.objects.filter(recruiter=recruiter).select_related('candidate', 'job')
+
+    _context = {'title': "Djinni - Inbox", 'recruiter': recruiter, 'threads': threads}
 
     return render(request, 'inbox/chats.html', _context)
 
+
 def inbox_thread(request, pk):
-    thread = MessageThread.objects.get(id = pk)
+    thread = MessageThread.objects.get(id=pk)
     messages = thread.message_set.all().order_by('created')
 
     _context = {
@@ -26,6 +28,33 @@ def inbox_thread(request, pk):
         'thread': thread,
         'messages': messages,
         'candidate': thread.candidate,
+        'score': calculate_match_score(thread.candidate, thread.job)
     }
 
     return render(request, 'inbox/thread.html', _context)
+
+
+def posted_jobs(request):
+    job_posting = JobPosting.objects.filter(recruiter=RECRUITER_ID).all()
+    _context = {'title': "Djinni - Jobs", 'jobPosting': job_posting}
+
+    return render(request, 'jobs/job_list.html', _context)
+
+
+def jobs_candidates(request, pk):
+    job = JobPosting.objects.filter(recruiter=RECRUITER_ID).get(id=pk)
+
+    rated_threads = [
+        {
+            'thread': thread,
+            'candidate': thread.candidate,
+            'score': calculate_match_score(thread.candidate, job)
+        }
+        for thread in MessageThread.objects.filter(job=job.id)
+    ]
+
+    _context = {'title': "Djinni - Candidates",
+                'job': job,
+                'rated_threads': sorted(rated_threads, key=lambda x: x['score'], reverse=True)}
+
+    return render(request, 'jobs/job_matches.html', _context)
